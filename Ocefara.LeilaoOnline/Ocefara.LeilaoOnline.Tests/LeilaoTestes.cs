e3feff297d8d41f1ba3b6ae9e781175c0de9d203
Rafa C <rafasantcont@yahoo.com.br>
2022-08-20 17:00:08 -0300

    Copiar os dois testes do projeto console para o xUnit

---
+++ Ocefara.LeilaoOnline/Ocefara.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -0,0 +1,50 @@
+﻿using Ocefara.LeilaoOnline.Core;
+using Xunit;
+
+namespace Ocefara.LeilaoOnline.Tests
+{
+    public class LeilaoTestes
+    {
+        [Fact]
+        private void LeilaoComVariosLances()
+        { 
+            // Arrange - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(fulano, 990);
+
+            // Act - método sob teste
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        private static void LeilaoComApenasUmLance()
+        {
+            // Arrange - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            // Act - método sob teste
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
